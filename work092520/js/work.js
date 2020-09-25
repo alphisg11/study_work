57470a1c84732c1e8395e3af67d81fa6c433a6a3
alphisg11 <1703493488@qq.com>
2020-09-25 21:46:14 +0800

    092520-webwork

--- work092520/js/work.js
+++ work092520/js/work.js
@@ -1,15 +1,15 @@
 //Work 1
 function cho() {
-	var a = document.getElementById("key"); //获取输入框的值
-	var k = ch(a.value);
+	var x = document.getElementById("key"); //获取输入框的值
+	var k = ch(x.value);
 
 }
 
-function ch(a) {
-	var k = a;
-	while(a > 1) { //递减阶乘
-		k = k * (a - 1);
-		a--;
+function ch(x) {
+	var k = x;
+	while(x > 1) { //递减阶乘
+		k = k * (x - 1);
+		x--;
 	}
 	document.getElementById("abs").value = k; //结果输出到输出框
 }
@@ -23,62 +23,54 @@ function tc() {
 }
 
 function time(a, b, c) {
+
 	if(a > 0 && a < 2100) { //年份检测在公元1年至公元2099年为合法年份
-		if(a % 4 == 0) { //首先检测是否为闰年
-			if(b < 1 || b > 13) {
-				untime();
-			}
-			if(b == 2) { //闰年的情况下先检测2月的日期是否合法
+		if(a % 4 == 0) { //检测平闰年
+			if(b == 2) {
 				if(c > 0 && c < 29) {
 					ontime();
 				} else {
 					untime();
 				}
 			}
-			if(b == 4 || b == 6 || b == 9 || b == 11) { //如果是4,6,9,11月，检测日期是否合法
-				if(c > 0 && c < 31) {
-					ontime();
-				} else {
-					untime();
-				}
-			}
-			if(b == 1 || b == 3 || b == 5 || b == 7 || b == 8 || b == 10 | b == 12) { //如果是1,3,5,7,8,10,12月，检测日期是否合法
-				if(c > 0 && c < 32) {
-					ontime();
-				} else {
-					untime();
-				}
-			}
+
+			monthx(b, c);
+
 		} else {
-			if(b < 1 || b > 13) {
-				untime();
-			}
-			if(b == 2) { //非闰年的情况下先检测2月的日期是否合法
+			if(b == 2) {
 				if(c > 0 && c < 30) {
 					ontime();
 				} else {
 					untime();
 				}
 			}
-			if(b == 4 || b == 6 || b == 9 || b == 11) { //同上
-				if(c > 0 && c < 31) {
-					ontime();
-				} else {
-					untime();
-				}
-			}
-			if(b == 1 || b == 3 || b == 5 || b == 7 || b == 8 || b == 10 | b == 12) { //同上
-				if(c > 0 && c < 32) {
-					ontime();
-				} else {
-					untime();
-				}
-			}
+
+			monthx(b, c);
 		}
 	} else {
 		untime();
 	}
 
+}
+
+function monthx(b, c) {
+	if(b < 1 || b > 13) { //检测月份是否合法
+		untime();
+	}
+	if(b == 4 || b == 6 || b == 9 || b == 11) { //检测4,6,9,11月份的日期是否合法
+		if(c > 0 && c < 31) {
+			ontime();
+		} else {
+			untime();
+		}
+	}
+	if(b == 1 || b == 3 || b == 5 || b == 7 || b == 8 || b == 10 | b == 12) { //检测1,3,5,7,8,10,12月份的日期是否合法
+		if(c > 0 && c < 32) {
+			ontime();
+		} else {
+			untime();
+		}
+	}
 }
 
 function untime() {
